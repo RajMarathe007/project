916908700a988b9a057258198dfa842942021308
Raj Marathe <89590258+RajMarathe007@users.noreply.github.com>
2022-12-04 04:11:19 +0530

    Create reports.php

---
+++ php/reports.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+  <title>Building Material Supplier</title>
+  <meta content="" name="description">
+  <meta content="" name="keywords">
+
+  <!-- Favicons -->
+  <link href="assets/img/logo.png" rel="icon">
+  <link href="assets/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+  <!-- Google Fonts -->
+  <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,600,600i,700,700i|Raleway:300,300i,400,400i,500,500i,600,600i,700,700i|Poppins:300,300i,400,400i,500,500i,600,600i,700,700i" rel="stylesheet">
+
+  <!-- Vendor CSS Files -->
+  <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+  <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+  <link href="assets/vendor/boxicons/css/boxicons.min.css" rel="stylesheet">
+  <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+  <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+  <!-- Template Main CSS File -->
+  <link href="assets/css/style.css" rel="stylesheet">
+
+  <!-- =======================================================
+  * Template Name: Scaffold - v4.7.0
+  * Template URL: https://bootstrapmade.com/scaffold-bootstrap-metro-style-template/
+  * Author: BootstrapMade.com
+  * License: https://bootstrapmade.com/license/
+  ======================================================== -->
+  <style>
+  .ar{
+background-image:url("background.png");
+background-size:100%;
+background-repeat:no-repeat;
+}
+</style>
+    </head>
+
+    <body class="ar">
+
+  <!-- ======= Header ======= -->
+  <header id="header" class="fixed-top d-flex align-items-center">
+    <div class="container d-flex align-items-center">
+
+      <div class="logo me-auto">
+        <!--<h1><a href="index.html">Material Suppliers</a></h1>-->
+		<a href="index.html"><img src="assets/img/logo3.png" alt="" class="img-fluid"></a></div>
+        <!-- Uncomment below if you prefer to use an image logo -->
+      </div>
+
+      <nav id="navbar" class="navbar order-last order-lg-0">
+        <ul>
+        <li><a class="nav-link scrollto active" href="index1.html">Home</a></li>
+          <li><a class="nav-link scrollto" href="print_customer.php">Customer Report</a></li>
+          <li><a class="nav-link scrollto" href="print_material.php">Material Report</a></li>
+          <li><a class="nav-link scrollto" href="print_order.php">Order Report</a></li>
+          <li><a class="nav-link scrollto" href="print_delivery.php">Delivery Report</a></li>
+          <li><a class="nav-link scrollto" href="index1.html">Back</a></li>
+
+        </ul>
+        <i class="bi bi-list mobile-nav-toggle"></i>
+      </nav><!-- .navbar -->
+    </div>
+  </header><!-- End Header -->
+<br><br><br><br><br><br>
+<center><h3>Select any one of the options from Menu</h3></center>
+</form>
+</body>
+</html>
